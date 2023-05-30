450c3fb201cb36e619b23457219dce697fdfbfd6
Psylops <hanansmani@gmail.com>
2023-05-30 06:56:41 -1000

    rebuilt dir

--- cutil_page/script.js
+++
@@ -1,9 +0,0 @@
-var OSName = "Unknown";
-if (window.navigator.userAgent.indexOf("Windows")!= -1) OSName=" Windows";
-if (window.navigator.userAgent.indexOf("Linux")!= -1) OSName="Linux"
-var osn = document.querySelector("osname")
-osn.value = "Windows";
-
-function download(){
-    alert("it works ig")
-}
