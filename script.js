17d037e16f88d2e5173f6a459634a4f7171e924e
Psylops <hanansmani@gmail.com>
2023-06-06 10:55:58 +0200

    [insert commit message here]

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const blob = document.getElementById('blob');
-let bWidth = blob.style.width / 2;
-let bHeight = blob.style.height / 2;
+
 document.addEventListener('mousemove', (e) => {
-    let bX = e.clientX - bWidth;
-    let bY = e.clientY - bHeight;
+
     blob.animate({
-        left: `${bX}px`,
-        top: `${bY}px`
+        left: `${e.clientX}px`,
+        top: `${e.clientY}px`
     }, {duration: 1500, fill: "forwards"});
 
 });
