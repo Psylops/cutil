00da49969fb0752553d2384d426b1624ccc61441
Psylops <hanansmani@gmail.com>
2023-06-06 09:03:34 +0200

    [insert good commit message here]

--- cutil_page/script.js
+++ cutil_page/script.js
@@ -1,9 +1,12 @@
 const blob = document.getElementById('blob');
+let bWidth = blob.style.width / 2;
+let bHeight = blob.style.height / 2;
 document.addEventListener('mousemove', (e) => {
-
+    let bX = e.clientX - bWidth;
+    let bY = e.clientY - bHeight;
     blob.animate({
-        left: `${(e.clientX + 50)}px`,
-        top: `${e.clientY}px`
+        left: `${bX}px`,
+        top: `${bY}px`
     }, {duration: 1500, fill: "forwards"});
 
 });
